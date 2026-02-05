15dba064ce7a12e9264f83f3957831ef702addeb
Polineaaa <polina20983@gmail.com>
2026-02-05 21:07:32 +0700

    Week 1: Added templates and mock data loop

---
+++ gallery/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+        fake_database =[
+            {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
+            {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
+            {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
+        ]
+        context_data = {
+        'page_title': 'Главная Галерея',
+        'assets': fake_database,
+        }
+        return render(request, 'gallery/index.html', context_data)
