ad6f3d36bf05d1699e98137c071f6329a120b20a
Polineaaa <polina20983@gmail.com>
2026-02-05 21:47:52 +0700

    Week 1: Added templates and mock data loop

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from gallery.views import home
+from gallery.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
